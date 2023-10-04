0b939a1c6863160eaec86ca5804585d039fa2b6c
ingdanielb <danielb2889@hotmail.com>
2023-10-04 06:55:09 -0500

    Base and API

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import json
 
 from horario import competenciasInstructor, horario, horario_fichas, instructores, fichas
 
@@ -22,6 +24,15 @@ def horario_ficha():
         horario_f = horario_fichas(ficha)
         return render_template("horario_ficha.html", horario_f=horario_f, listado_fichas=listado_fichas)    
     return render_template("horario_ficha.html", listado_fichas=listado_fichas)
+
+@app.route("/api/h_instructor", methods=["GET", "POST"])
+def api_h_instructores():
+    horarioInstructor = horario('DANIEL DAVID BENAVIDES SÁNCHEZ ')
+    horario_dict = horarioInstructor.copy()
+    horario_dict['horario'] = [(k, v) for k, v in horario_dict['horario'].items()]
+    horario_json = json.dumps(horario_dict, default=lambda x: x.tolist() if isinstance(x, pd.Series) else x)
+    """ horario_dict = json.loads(horario_json) """
+    return horario_json
 
 if __name__ == "__main__":
     app.run(debug=True)
